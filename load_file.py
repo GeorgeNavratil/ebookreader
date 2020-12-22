a36b9f6970d89e0b5d6ab9a233d0fd543cf74b41
Tom <33488764+TomasKricka@users.noreply.github.com>
2020-12-22 09:09:43 +0100

    pdf file loader is working

--- load_file.py
+++ load_file.py
@@ -1,15 +1,48 @@
 from tkinter import *
-from PIL import ImageTk,Image, 
 from tkinter import filedialog
-from PyPDF2 import PdfFileReader
+import PyPDF2
 
 root = Tk()
 root.title('loudim jak pan')
+root.geometry("500x500")
 
-def open():
-    global my_file
-    root.filename = filedialog.askopenfilename(initialdir="D:\Kódování\ebook-reader", title = "Find a file",filetypes=(("pdf files", "*.pdf"),("all files", "*.*")))
-    my_label = Label(root, text = root.filename).pack()
-    my_file = PdfFileReader()
+#Create text box
+my_text = Text(root, height=30, width=60)
+my_text.pack(pady=10)
+
+#clear text box
+def clear_text_box():
+    my_text.delete(1.0, END)
+
+#open pdf file
+def open_pdf():
+    #grab the filename of the pdf file
+    open_file = filedialog.askopenfilename(initialdir = "D:\Kódování\ebook-reader", title = "Open PDF file", filetypes = (("pdf file", "*.pdf"), ("All files", "*.*")))
+
+    #check to see of there os a file
+    if open_file:
+        # open the pdf file
+        pdf_file = PyPDF2.PdfFileReader(open_file)
+        #set the page to read
+        page = pdf_file.getPage(0)
+        #extract the text from the pdf file
+        page_stuff = page.extractText()
+        # add text to text box
+        my_text.insert(1.0, page_stuff)
+
+
+#create menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+#add some dropdown menus
+file_menu = Menu(my_menu, tearoff = False)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Open file", command = open_pdf)
+file_menu.add_command(label="Clear", command = clear_text_box)
+file_menu.add_command(label="Exit", command = root.quit)
+
+
+
 
 root.mainloop()
