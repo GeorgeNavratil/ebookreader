27c70e12b06c36b0f6fe7c3a430fe74bccdd53c2
Tom <33488764+TomasKricka@users.noreply.github.com>
2020-12-22 12:27:04 +0100

    move testing files, add pdf files, update load_file.py

--- load_file.py
+++ load_file.py
@@ -1,25 +1,46 @@
 from tkinter import *
 from tkinter import filedialog
 import PyPDF2
+from tika import parser
 
 root = Tk()
 root.title('loudim jak pan')
 root.geometry("500x500")
 
 #Create text box
-my_text = Text(root, height=30, width=60)
+my_text = Text(root, height=50, width=60)
 my_text.pack(pady=10)
 
 #clear text box
 def clear_text_box():
+    my_text.configure(state=NORMAL)
     my_text.delete(1.0, END)
 
 #open pdf file
 def open_pdf():
-    #grab the filename of the pdf file
-    open_file = filedialog.askopenfilename(initialdir = "D:\Kódování\ebook-reader", title = "Open PDF file", filetypes = (("pdf file", "*.pdf"), ("All files", "*.*")))
+    my_text.delete(1.0, END)
 
-    #check to see of there os a file
+    '''
+    #grab the filename of the pdf file
+    open_file = filedialog.askopenfilename(
+            initialdir = "D:\Kódování\ebook-reader", 
+            title = "Open PDF file",
+            filetypes = (("pdf file", "*.pdf"), 
+                        ("All files", "*.*"))
+            )
+    '''
+
+    new_file = parser.from_file("D:\Kódování\ebook-reader\pdf3.pdf")
+    new_file = str(new_file)
+
+    safe_text = new_file.encode('utf-8', errors='ignore')
+    safe_text = str(safe_text).replace('\n', '').replace('\\', '')
+
+    my_text.insert(1.0, safe_text)
+
+    
+    '''
+    #check to see if there is a file
     if open_file:
         # open the pdf file
         pdf_file = PyPDF2.PdfFileReader(open_file)
@@ -29,13 +50,16 @@ def open_pdf():
         page_stuff = page.extractText()
         # add text to text box
         my_text.insert(1.0, page_stuff)
+    '''
+
+    my_text.configure(state=DISABLED)
 
 
 #create menu
 my_menu = Menu(root)
 root.config(menu=my_menu)
 
-#add some dropdown menus
+#add dropdown menus
 file_menu = Menu(my_menu, tearoff = False)
 my_menu.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="Open file", command = open_pdf)
