5d5e2ec5c8adea698a74fc3bf6e0a38b430d29b1
Tom <33488764+TomasKricka@users.noreply.github.com>
2020-12-28 20:59:44 +0100

    moving with pages is working (breaks after last page), number page label is working, added  pdf and docx file with 7 pages

--- load_file.py
+++ load_file.py
@@ -6,12 +6,15 @@ import os
 
 root = Tk()
 root.title('Ebook Reader')
-root.geometry("1000x700")
+root.geometry("1400x700")
+
+#page number init
+page_number_label = 1
 
 #Create text box
 my_text = Text(root, height=30, width=120)
 my_text.pack(pady=10)
-my_text.grid(row=0, column=0)
+my_text.grid(row=0, column=0, columnspan=7)
 
 #clear text box
 def clear_text_box():
@@ -30,13 +33,14 @@ def open_pdf():
                         ("All files", "*.*"))
             )
     
+    global doc
     doc = fitz.open(open_file)
     #print ("number of pages: %i" % doc.pageCount)
     #print(doc.metadata)
 
-    page1 = doc.loadPage(0)
-    page1text = page1.getText("text")
-    my_text.insert(1.0, page1text)
+    page_content = doc.loadPage(0)
+    page_text = page_content.getText("text")
+    my_text.insert(1.0, page_text)
 
     '''
     #check to see if there is a file
@@ -54,6 +58,88 @@ def open_pdf():
     my_text.configure(state=DISABLED)
 
 
+    #indentation instead of .pack() which isnt working for some reason
+    indentation_label = Label(text=" ")
+    indentation_label.grid(row=1)
+
+    #page number init
+    updateLabel(page_number_label)
+
+    #forward, back buttons
+    button_forward = Button(root, text=">>", height=1, width=10, command=lambda: forward(1))
+    '''button_forward.pack(pady=10)'''
+    button_forward.grid(row=2, column=5)
+
+    button_back = Button(root, text="<<", height=1, width=10, state=DISABLED)
+    '''button_back.pack(pady=10)'''
+    button_back.grid(row=2, column=1)
+
+
+def updateLabel(current_page):
+    number_of_pages = doc.pageCount
+    
+    #page number
+    page_label = Label(text="Page: " + str(current_page) + "/" + str(number_of_pages))
+    page_label.grid(row=2, column=3)
+
+
+def forward(page_number):
+    global my_text
+    global page_number_label
+    global button_forward
+    global button_back
+
+    my_text.configure(state=NORMAL)
+    my_text.delete(1.0, END)
+
+    page_content = doc.loadPage(page_number)
+    page_text = page_content.getText("text")
+    my_text.insert(1.0, page_text)
+
+    my_text.configure(state=DISABLED)
+
+    button_forward = Button(root, text=">>", height=1, width=10, 
+                            command=lambda: forward(page_number+1))
+    button_back = Button(root, text="<<", height=1, width=10, 
+                            command=lambda: back(page_number-1))
+    
+    '''if'''
+
+    button_forward.grid(row=2, column=5)
+    button_back.grid(row=2, column=1)
+
+    page_number_label = page_number_label + 1
+    updateLabel(page_number_label)
+
+def back(page_number):
+    global my_text
+    global page_number_label
+    global button_forward
+    global button_back
+
+    my_text.configure(state=NORMAL)
+    my_text.delete(1.0, END)
+
+    page_content = doc.loadPage(page_number)
+    page_text = page_content.getText("text")
+    my_text.insert(1.0, page_text)
+
+    my_text.configure(state=DISABLED)
+
+    button_forward = Button(root, text=">>", height=1, width=10, 
+                            command=lambda: forward(page_number+1))
+    button_back = Button(root, text="<<", height=1, width=10, 
+                            command=lambda: back(page_number-1))
+    
+    '''if'''
+
+    button_forward.grid(row=2, column=5)
+    button_back.grid(row=2, column=1)
+    
+    page_number_label = page_number_label - 1
+    updateLabel(page_number_label)
+
+
 #create menu
 my_menu = Menu(root)
 root.config(menu=my_menu)
@@ -63,8 +149,8 @@ file_menu = Menu(my_menu, tearoff = False)
 my_menu.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="Open file", command = open_pdf)
 
-#file_menu.add_command(label="Save file", command = save_file)
-#file_menu.add_command(label="Edit", command = edit)
+'''file_menu.add_command(label="Save file", command = save_file)
+file_menu.add_command(label="Edit", command = edit)'''
 
 file_menu.add_command(label="Clear", command = clear_text_box)
 file_menu.add_command(label="Exit", command = root.quit)
